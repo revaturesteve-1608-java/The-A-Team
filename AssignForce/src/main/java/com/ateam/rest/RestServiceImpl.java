86eca3633fb24342cedda3aad12617bd14c9772a
Zachary Curtis <curt3897@alumni.uidaho.edu>
2016-10-07 16:14:16 -0400

    fixed jdbc

--- AssignForce/src/main/java/com/ateam/rest/RestServiceImpl.java
+++ AssignForce/src/main/java/com/ateam/rest/RestServiceImpl.java
@@ -2,6 +2,7 @@ package com.ateam.rest;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.core.ParameterizedTypeReference;
@@ -20,7 +21,7 @@ import com.ateam.domain.Unavailable;
 public class RestServiceImpl implements RestService{
 
 	//put this on a random server application for the time being
-	private static final String BASE_URL  = "http://localhost:8080/AssignForce/resources/js/json";
+	private static final String BASE_URL  = "http://54.165.139.13:2403";
 	// Zach's local
 //	private static final String BASE_URL  = "C:/Users/Zach/Repositories/The-A-Team/AssignForce/src/main/webapp/resources/js/json";
 	
@@ -30,8 +31,8 @@ public class RestServiceImpl implements RestService{
 		RestTemplate restTemplate = new RestTemplate();
 		URI url = null;
 		//append different end to url based on input type
-		System.out.println("uhhh");
 		String tail = checkType(type);
+		System.out.println("uhhh");
 		
 		try {
 			url = new URI(BASE_URL + tail);
@@ -47,7 +48,10 @@ public class RestServiceImpl implements RestService{
 		
 		//puts the response in a list
 		List<T> respList = rateResponse.getBody();
-
+		
+		respList = convertResponseToList(type, respList);
+		
+		System.out.println("resplist: " + respList.get(0).getClass());
 		// not sure this is correct?
 //		type = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", (T) type.getClass());
 //		type = restTemplate.getForObject(url, (Class<T>) type.getClass());
@@ -55,12 +59,25 @@ public class RestServiceImpl implements RestService{
 		return respList;
 	}
 	
+	private static <T,R> List<T> convertResponseToList(T sample, List<R> respList){
+		List<T> outList = new ArrayList<T>();
+		
+		for (R r : respList) {
+			outList.add((T) r);
+		}
+		System.out.println("respList: " + respList);
+		System.out.println("outList: " + outList);
+		return outList;
+	}
+	
+	
+	
 	@SuppressWarnings("unchecked")
 	private static <T> String checkType(T type){
 		//TODO 
 		// need proper URLs for the test data
 		if((Class<T>) type.getClass() == Trainer.class){
-			return "/TrainerJSON.json";
+			return "/trainers";
 		}
 		else if((Class<T>) type.getClass() == Unavailable.class){
 			return  "/UnavailableJSON.json";
--- AssignForce/src/test/java/com/ateam/driver/Driver.java
+++ AssignForce/src/test/java/com/ateam/driver/Driver.java
@@ -1,10 +1,12 @@
 package com.ateam.driver;
 
+import org.springframework.stereotype.Component;
+
 import com.ateam.domain.Trainer;
 import com.ateam.rest.RestServiceImpl;
 import com.ateam.service.InitServiceImpl;
 
-
+@Component
 public class Driver {
 
 	public static void main(String[] args) {
@@ -14,8 +16,8 @@ public class Driver {
 		InitServiceImpl isi = new InitServiceImpl();
 		RestServiceImpl rsi = new RestServiceImpl();
 		
-		rsi.getItemFromRest(new Trainer());
-//		isi.initSetup();// tests the init setup pieces
+//		rsi.getItemFromRest(new Trainer());
+		isi.initSetup();// tests the init setup pieces
 		
 	}
 
