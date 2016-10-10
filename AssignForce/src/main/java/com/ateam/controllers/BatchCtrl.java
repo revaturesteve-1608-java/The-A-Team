1e163c1034ceabe2723a705ddc2d10153ff3dd49
Sean Veal <seand.veal@yahoo.com>
2016-10-10 10:32:39 -0400

    Sean's commit--little changes

---
+++ AssignForce/src/main/java/com/ateam/controllers/BatchCtrl.java
@@ -0,0 +1,16 @@
+package com.ateam.controllers;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ateam.domain.Curriculum;
+
+@RestController
+public class BatchCtrl {
+
+	public ResponseEntity<List<Curriculum>> getCurriculums(){
+		
+	}
+}
