cea476c5440799b813e992eaaddc064b67ecc3a5
Zachary Curtis <curt3897@alumni.uidaho.edu>
2016-10-11 11:14:01 -0400

    added ability to grab lists of curriculums

--- AssignForce/src/main/java/com/ateam/controllers/BatchCtrl.java
+++ AssignForce/src/main/java/com/ateam/controllers/BatchCtrl.java
@@ -2,15 +2,30 @@ package com.ateam.controllers;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ateam.domain.Curriculum;
+import com.ateam.service.DaoService;
 
 @RestController
 public class BatchCtrl {
 
-	public ResponseEntity<List<Curriculum>> getCurriculums(){
-		return null;
+	@Autowired
+	DaoService daoService;
+
+	@RequestMapping(value = { "/curriculum"}, 
+			method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_VALUE, 
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Curriculum>> getCurriculums() {
+
+		List<Curriculum> re = daoService.getAllItem(new Curriculum());
+
+		return new ResponseEntity<List<Curriculum>>(re, HttpStatus.OK);
 	}
 }
